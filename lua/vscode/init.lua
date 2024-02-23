4d8c002784f4f4adb46807c59bb26ec99b8ad72d
saberzero1 <ewjbangma@hotmail.com>
2024-02-23 08:31:53 +0100

    Updated config

--- lua/vscode/init.lua
+++ lua/vscode/init.lua
@@ -1,4 +1,9 @@
 local opt = vim.opt
 local g = vim.g
+local vscode = require("vscode-neovim")
 
 g.mapleader = " "
+
+vim.notify = vscode.notify
+
+vscode.notify("Neovim config loaded successfully!")
